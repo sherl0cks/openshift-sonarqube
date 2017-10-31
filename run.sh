9cc906e2cc6fe53e6c133b8e245acf08aa73efb3
Justin Holmes <jholmes@redhat.com>
2017-10-30 23:33:21 -0400

    commenting out stuff where permissions could be a problem

--- run.sh
+++ run.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-set -x
-set -e
+# set -x
+# set -e
 
-## If the mounted data volume is empty, populate it from the default data
-if ! [[ "$(ls -A /opt/sonarqube/data)" ]]; then
-    cp -a /opt/sonarqube/data-init /opt/sonarqube/data
-fi
+# ## If the mounted data volume is empty, populate it from the default data
+# if ! [[ "$(ls -A /opt/sonarqube/data)" ]]; then
+#     cp -a /opt/sonarqube/data-init /opt/sonarqube/data
+# fi
 
-## If the mounted extensions volume is empty, populate it from the default data
-if ! [[ -d /opt/sonarqube/data/plugins ]]; then
-	cp -a /opt/sonarqube/extensions-init/plugins /opt/sonarqube/data/plugins
-fi
+# ## If the mounted extensions volume is empty, populate it from the default data
+# if ! [[ -d /opt/sonarqube/data/plugins ]]; then
+# 	cp -a /opt/sonarqube/extensions-init/plugins /opt/sonarqube/data/plugins
+# fi
 
-## Link the plugins directory from the mounted volume
-rm -rf /opt/sonarqube/extensions/plugins
-ln -s /opt/sonarqube/data/plugins /opt/sonarqube/extensions/plugins
+# ## Link the plugins directory from the mounted volume
+# rm -rf /opt/sonarqube/extensions/plugins
+# ln -s /opt/sonarqube/data/plugins /opt/sonarqube/extensions/plugins
 
-if [ "${1:0:1}" != '-' ]; then
-  exec "$@"
-fi
+# if [ "${1:0:1}" != '-' ]; then
+#   exec "$@"
+# fi
 
-#chown -R 0:0 /opt/sonarqube && chmod -R g+rwX /opt/sonarqube
+# #chown -R 0:0 /opt/sonarqube && chmod -R g+rwX /opt/sonarqube
 
-set
+# set
 
 java -jar lib/sonar-application-$SONAR_VERSION.jar \
     -Dsonar.web.javaAdditionalOpts="${SONARQUBE_WEB_JVM_OPTS} -Djava.security.egd=file:/dev/./urandom" \
